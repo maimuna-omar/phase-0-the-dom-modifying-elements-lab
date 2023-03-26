5c7cf006525fa3c17343e912ebd5b88035696985
maimuna omar <maymunaabdi07@gmail.com>
2023-03-26 15:18:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+// Select the 'main' element by its ID and remove it from the DOM
+const main = document.querySelector('#main');
+main.remove();
+
+// Create a new 'h1' element with the text "YOUR-NAME is the champion"
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "YOUR-NAME is the champion";
+
+// Append the new 'h1' element to the document's 'body' element
+document.body.appendChild(newHeader);
